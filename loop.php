aa5d41089bf00427968b462a5a90f9bd3a0f773c
Bishal <bishal1831047@gmail.com>
2022-04-29 21:57:45 +0600

    third commit

---
+++ loop.php
@@ -0,0 +1,53 @@
+<?php 
+
+    $num=1;
+
+    while($num<50){
+
+        ++$num;
+
+        echo $num ."</br>"; 
+
+        
+    }
+
+    $number = 1;
+
+    while($number>=-34){
+        $number--;
+
+        echo $number;
+    }
+
+    $numb =0;
+    do {
+        $numb ++;
+        
+        echo $numb . "</br>";
+    }while($numb<=20);
+
+
+    for ($a=30; $a>=-30; $a--) {
+
+        echo $a;
+    }
+
+
+    $many = array ("first","second","third");
+
+
+    foreach($many as $single){
+
+        echo $single . "</br>";
+    }
+
+
+
+
+
+
+
+
+
+
+?>
